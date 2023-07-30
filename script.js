db6588c52088dcff6a7431de0dcf727d25a9e489
Saif-09 <saifmd238@gmail.com>
2023-07-30 06:39:21 +0530

    locomotive added in code for smooth scrolling effect

---
+++ script.js
@@ -0,0 +1,5 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+//el refers to the element, Element on which locomotive has to be used. In our case we have to use in whole website so I selected #main.
